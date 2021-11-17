44a105cdf8b0399e59750fe2ec791dd5a5b04e90
brteo <51654355+brteo@users.noreply.github.com>
2021-11-17 16:54:34 +0100

    Refactor ShotView with ShotYellow trait

---
+++ src/main/scala/lns/scenes/game/shot/ShotYellow.scala
@@ -0,0 +1,27 @@
+package lns.scenes.game.shot
+
+import indigo.*
+import indigo.shared.scenegraph.{ Graphic, Shape }
+import lns.core.Animations.*
+import lns.core.*
+import lns.scenes.game.anything.*
+
+/**
+ * Isaac Character view elements builder
+ */
+trait ShotYellow extends AnythingAsset {
+  override val name: String  = ""
+  override val width: Int    = 40
+  override val height: Int   = 40
+  override val offsetY: Int  = 0
+  override val scale: Double = 1
+
+  def shot: Shape =
+    Shape
+      .Circle(
+        center = Point(width / 2, height / 2),
+        radius = width / 2,
+        fill = Fill.Color(RGBA(1, 1, 0, 1)),
+        stroke = Stroke(2, RGBA.Black)
+      )
+}
