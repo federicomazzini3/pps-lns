43fbea6d13a23f4734ad376dcf71c7d9a714baf5
federicomazzini3 <federico.mazzini3@studio.unibo.it>
2021-11-29 11:49:24 +0100

    update doc and comments

--- src/main/scala/lns/scenes/game/room/Floor.scala
+++ src/main/scala/lns/scenes/game/room/Floor.scala
@@ -1,9 +1,11 @@
 package lns.scenes.game.room
 
 import lns.core.Assets
-
 import scala.util.Random
 
+/**
+ * Series of method to create and manage a floor inside a room
+ */
 object Floor {
   val n: Int       = (Assets.Rooms.floorSize / Assets.Rooms.cellSize).toInt
   val nCenter: Int = n - 1
@@ -11,4 +13,11 @@ object Floor {
   val random: Cell = Cell(Random.between(0, n), Random.between(0, n))
 }
 
+/**
+ * Model for a cell that compose floor
+ * @param x
+ *   the column number in grid
+ * @param y
+ *   the row number in grid
+ */
 case class Cell(x: Int, y: Int)
--- src/main/scala/lns/scenes/game/subsystems/BattleModel.scala
+++ src/main/scala/lns/scenes/game/subsystems/BattleModel.scala
@@ -7,43 +7,107 @@ import lns.scenes.game.anything.AnythingModel
 import lns.scenes.game.characters.CharacterModel
 import indigo.*
 
+/**
+ * Model for BattleEventSubsystem
+ * @param bc
+ *   collection of battleconsequence (hurt or dead for an anything)
+ * @param state
+ *   state of the game
+ */
 case class BattleModel(bc: Set[BattleConsequence], state: GameState) {
+
+  /**
+   * Update a battle model
+   * @param f
+   *   strategy of update
+   * @return
+   *   a new updated BattleModel
+   */
   def update(f: BattleModel => Outcome[BattleModel]): Outcome[BattleModel] = f(this)
 
+  /**
+   * Remove all BattleConsequence older than 360 milliseconds ago.
+   * @param context
+   *   the context of subsystem that store GameTime
+   * @return
+   *   a new updated BattleModel
+   */
   def expireBattleConsequence(context: SubSystemFrameContext): Outcome[BattleModel] =
     this.update(battleModel =>
       Outcome(battleModel.copy(bc = bc.filter(bc => bc.timeElapse(context.gameTime, Seconds(0.360)))))
     )
 
+  /**
+   * Add a BattleConsequence to the collection
+   * @param toAddBC
+   *   the BattleConsequence to add
+   * @return
+   *   a new updated BattleModel
+   */
   def addBattleConsequence(toAddBC: BattleConsequence): Outcome[BattleModel] =
     this.update(battleModel => Outcome(this.copy(bc = bc + toAddBC)))
 
+  /**
+   * Update the state of BattleModel with game over
+   * @return
+   *   a new updated BattleModel
+   */
   def gameOver: Outcome[BattleModel] =
     this.update(battleModel => Outcome(this.copy(state = GameOver))).addGlobalEvents(Redirect(500))
 
+  /**
+   * Update the state of BattleModel with win
+   * @return
+   *   a new updated BattleModel
+   */
   def win: Outcome[BattleModel] =
     this.update(battleModel => Outcome(this.copy(state = Win))).addGlobalEvents(Redirect(500))
 }
 
+/**
+ * Companion object of BattleModel
+ */
 object BattleModel {
   def initial = BattleModel(Set.empty, Play)
 }
 
+/**
+ * Trait for a battle consequence. A battle consequence is a consequence of a shot that it an Anything That can be a
+ * Hurt or a Dead
+ */
 trait BattleConsequence {
   val a: AnythingModel
   val gameTime: GameTime
 
+  /**
+   * Time between the creation of this BattleConsequence and another moment
+   * @param other
+   *   the other time to compare
+   * @param seconds
+   *   the seconds between the two to compare
+   * @return
+   */
   def timeElapse(other: GameTime, seconds: Seconds): Boolean =
     other.running.-(gameTime.running) < seconds
 }
+
 case class Hurt(a: AnythingModel, gameTime: GameTime) extends BattleConsequence
 case class Rip(a: AnythingModel, gameTime: GameTime)  extends BattleConsequence
 
+/**
+ * Trait that model the state of the game A game can be
+ *   - in playing
+ *   - Win
+ *   - Game over
+ */
 trait GameState
 case object Win      extends GameState
 case object GameOver extends GameState
 case object Play     extends GameState
 
+/**
+ * Event that BattleModel and his subsystem manage
+ */
 trait BattleEvent                 extends GlobalEvent { val a: AnythingModel }
 case class Hit(a: AnythingModel)  extends BattleEvent
 case class Dead(a: AnythingModel) extends BattleEvent
